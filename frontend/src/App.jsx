1b3d9b8f977a20cd47cf1918401f59032a0f2858
Matilde <misabelpcarmo@gmail.com>
2026-07-17 13:00:08 +0100

    Feat: add Loading screen

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import './Style.css'
 import Home from './screens/Home'
+import Loading from './screens/Loading'
 import Question from './screens/Question'
 import Results from './screens/Results'
 
@@ -11,6 +12,7 @@ function App() {
       <Home />
       <Question />
       <Results />
+      <Loading />
     </div>
   )
 }
---
+++ frontend/src/screens/Loading.jsx
@@ -0,0 +1,10 @@
+function Loading () {
+    return (
+        <div>
+            <img src="/assets/loader.gif" alt="loading-gif" />
+            <p>We're running to get your results.</p>
+        </div>
+    )
+}
+
+export default Loading
