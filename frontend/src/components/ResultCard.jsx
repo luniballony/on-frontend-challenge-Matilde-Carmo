0be785c075b2a9a00255f5a257a533d8f43e8f15
Matilde <misabelpcarmo@gmail.com>
2026-07-20 20:46:49 +0100

    Feat: add swatches feature
    - add swatches and 'fake' data to mimick swatches and visualize them

--- frontend/src/components/ResultCard.jsx
+++ frontend/src/components/ResultCard.jsx
@@ -1,6 +1,14 @@
 
 function ResultCard ({shoe}) {
 
+    // temporary data set to mimick the swatch setcion
+    const shadeSet = [
+        {name: 'Grey', primary: '#797979', secondary: '#c8e42d'},
+        {name: 'Moss', primary: '#1e5053', secondary: '#cc6332'},
+        {name: 'Blue', primary: '#21255f', secondary: '#5d7ed6'},
+        {name: 'Black', primary: '#0e0404', secondary: '#5c5757'}
+    ]
+
     return (
         <div className="results-card">
             <div className="result-info"> 
@@ -10,9 +18,18 @@ function ResultCard ({shoe}) {
                 <p>Your perfect partner in the world's lightest fully-cushioned shoe for Running Remixed.</p>
                 <div className="result-price-shade-box">    
                     <p className="result-price">200 CHF</p>
-                    <p className="result-shade-name">Neon & Grey</p>
+                    <p className="result-shade-name">Neon & Grey</p> {/* This would ideally take the actual shoe shade */}
                 </div>
-                <p>SHADES</p>
+                <div className="swatches">
+                    {
+                        shadeSet.map((color) => (
+                            <div className="swatch-set" key={color.name}>
+                                <div className="primary-swatch" style={{ backgroundColor: color.primary }} />
+                                <div className="secondary-swatch" style={{ backgroundColor: color.secondary }} />
+                            </div>
+                        ))
+                    }     
+                </div>      
             </div>
             <a href="https://www.on.com/en-pt/shop/shoes/running" target="_blank" className="btn-shop-now">Shop now</a>
         </div>
