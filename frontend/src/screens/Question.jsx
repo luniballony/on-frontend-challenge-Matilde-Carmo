c5d109bf8ef15943882c9ece003e608407ed2047
Matilde <misabelpcarmo@gmail.com>
2026-07-20 13:29:29 +0100

    Style: Question Page

--- frontend/src/screens/Question.jsx
+++ frontend/src/screens/Question.jsx
@@ -4,14 +4,14 @@ function Question ({ questionId , handleAnswer}) {
     const question = data.questions[questionId];
     
     return(
-        <div>
+        <div className="question-page">
             <div className="question-info">
                 <h3>TRY ON QUIZ</h3>
                 <h3>30 DAYS RISK FREE</h3>
             </div>
             <div>
                 <p>{question.copy}</p>
-                <div>
+                <div className="btn-box">
                     <button onClick={() => handleAnswer(0)}
                         >{question.answers[0].copy}</button>
 
