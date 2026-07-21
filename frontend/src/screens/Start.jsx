6709fb52fcba340f13e01b0a0f9fa20ee45c4028
Matilde <misabelpcarmo@gmail.com>
2026-07-21 17:59:05 +0100

    Style: Start Screen
    - improve on start Screen text and buttons

--- frontend/src/screens/Start.jsx
+++ frontend/src/screens/Start.jsx
@@ -1,13 +1,15 @@
 function Start ({startQuiz}) {
         return (
         <div className="start-screen">
-            <img src="/assets/Background Image Start Screen.png" alt="Background Image Start Screen" />
-
             <div className="home-info">
                 <h1>Take the quiz and try your first pair!</h1>
                 <button id="btn-start" onClick={startQuiz}>
                         Try On Trial</button>
                 <h3>30 Days risk free</h3>
+            </div>
+
+            <div className="image-container">
+                <img src="/assets/Background Image Start Screen.png" alt="Background Image Start Screen" />
             </div>
             
         </div>
