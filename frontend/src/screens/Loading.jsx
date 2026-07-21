eb445505db4536308f62be0d32cf32846b607619
Matilde <misabelpcarmo@gmail.com>
2026-07-21 20:01:07 +0100

    Style: made Loading Screen Gif smaller

--- frontend/src/screens/Loading.jsx
+++ frontend/src/screens/Loading.jsx
@@ -1,7 +1,7 @@
 function Loading () {
     return (
         <div className="loading-screen">
-            <img src="/assets/loader.gif" alt="loading-gif" />
+            <img src="/assets/loader.gif" alt="Loading Gif" className="loading-gif" />
             <p>We're running to get your results.</p>
         </div>
     )
