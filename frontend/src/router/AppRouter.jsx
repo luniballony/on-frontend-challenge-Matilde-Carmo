b0e701d095a10c263d999f4836b3833629c0b153
Matilde <misabelpcarmo@gmail.com>
2026-07-28 14:36:36 +0100

    Feat: add nav to Results

--- frontend/src/router/AppRouter.jsx
+++ frontend/src/router/AppRouter.jsx
@@ -2,17 +2,19 @@ import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import Start from '../screens/Start'
 import NotFound from '../screens/NotFound';
 import Question from "../screens/Question";
+import Results from '../screens/Results';
 import useQuiz from "../hooks/useQuiz";
 
 function AppRouter () {
 
-    const {phase, setPhase, questionId, handleAnswer, startQuiz, shoeScore, isLoading} = useQuiz();
+    const {questionId, handleAnswer, startQuiz, shoeScore} = useQuiz();
  
     return (
         <BrowserRouter>
             <Routes>
                 <Route path='/' element={<Start startQuiz={startQuiz} className="content" />} />
                 <Route path="/quiz" element={<Question questionId={questionId} handleAnswer={handleAnswer} />} />
+                <Route path="/results" element={<Results startQuiz={startQuiz} shoeScore={shoeScore} className="content" />} />
                 <Route path='/*' element={<NotFound />} />
             </Routes>
         </BrowserRouter>
