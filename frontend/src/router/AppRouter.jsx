042e439fb8cc5075ac41319d12dcd8b462f54b29
Matilde <misabelpcarmo@gmail.com>
2026-07-28 14:23:42 +0100

    Feat: add nav to Quiz
    - add nav to questions with /quiz

--- frontend/src/router/AppRouter.jsx
+++ frontend/src/router/AppRouter.jsx
@@ -1,13 +1,18 @@
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import Start from '../screens/Start'
 import NotFound from '../screens/NotFound';
+import Question from "../screens/Question";
+import useQuiz from "../hooks/useQuiz";
 
 function AppRouter () {
 
+    const {phase, setPhase, questionId, handleAnswer, startQuiz, shoeScore, isLoading} = useQuiz();
+ 
     return (
         <BrowserRouter>
             <Routes>
-                <Route path='/' element={<Start />} />
+                <Route path='/' element={<Start startQuiz={startQuiz} className="content" />} />
+                <Route path="/quiz" element={<Question questionId={questionId} handleAnswer={handleAnswer} />} />
                 <Route path='/*' element={<NotFound />} />
             </Routes>
         </BrowserRouter>
