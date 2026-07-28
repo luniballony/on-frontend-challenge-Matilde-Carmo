56de5e2bed8fcc656d2d78ef4958346dcdbe38b4
Matilde <misabelpcarmo@gmail.com>
2026-07-28 15:01:33 +0100

    Feat: logic to show Results
    - implementation of logic to move from Quiz to Results

--- frontend/src/router/AppRouter.jsx
+++ frontend/src/router/AppRouter.jsx
@@ -1,16 +1,19 @@
-import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useNavigate} from 'react-router-dom';
 import Start from '../screens/Start'
 import NotFound from '../screens/NotFound';
 import Question from "../screens/Question";
 import Results from '../screens/Results';
 import useQuiz from "../hooks/useQuiz";
+import { useEffect } from 'react';
 
 function AppRouter () {
 
-    const {questionId, handleAnswer, startQuiz, shoeScore} = useQuiz();
+    const {phase, questionId, handleAnswer, startQuiz, shoeScore} = useQuiz();
+
  
     return (
         <BrowserRouter>
+            <EndQuizNav phase={phase} />
             <Routes>
                 <Route path='/' element={<Start startQuiz={startQuiz} className="content" />} />
                 <Route path="/quiz" element={<Question questionId={questionId} handleAnswer={handleAnswer} />} />
@@ -20,6 +23,20 @@ function AppRouter () {
         </BrowserRouter>
     )
 
+}
+
+
+// since there's no direct way to go from quiz to results,
+// useNavigate listens for changes in phase
+function EndQuizNav ({phase}) {
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (phase === 'results') navigate('/results');
+    }, [phase, navigate]);
+    
+    return null;
 }
 
 export default AppRouter
